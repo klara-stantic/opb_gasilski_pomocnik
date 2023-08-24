31c8990153a60cfbd31bd9fe5b4e2691d596a518
Tine Fabiani <fabijot@gmail.com>
2023-08-24 22:51:18 +0200

    delo z napakami

--- py_code/spletni_vmesnik.py
+++ py_code/spletni_vmesnik.py
@@ -19,8 +19,11 @@ DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 # Database dostop
 conn_string = "host = '{0}' dbname = '{1}' user = '{2}' password = '{3}'".format(host, dbname, user, password)
 
+
+
 @get('/')
 def osnovna_stran():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             clani = cur.execute("""SELECT ime,priimek FROM clan WHERE aktiven = true AND (CURRENT_DATE-zdravniski) >=630 ORDER BY priimek""")
@@ -41,12 +44,13 @@ def osnovna_stran():
             tip_tek = cur.fetchall()
             tip_v = cur.execute("""SELECT * FROM tip_vozila""")
             tip_v = cur.fetchall()
-    return template('osnovna_stran.html',clani_n=clani_n,vozila_n=vozila_n,tip_v=tip_v,clani=clani,vozila=vozila,tek=tek,vaje=vaje,tip_tek=tip_tek,tip_vaje=tip_vaje)
+    return template('osnovna_stran.html',napaka=napaka,clani_n=clani_n,vozila_n=vozila_n,tip_v=tip_v,clani=clani,vozila=vozila,tek=tek,vaje=vaje,tip_tek=tip_tek,tip_vaje=tip_vaje)
 
 
 ###############################################################################
 @get('/clani/') 
 def prikaz_clanov():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             clani = cur.execute("""SELECT emso,ime,priimek,funkcija,cin,zdravniski,aktiven FROM clan WHERE aktiven = true  ORDER BY ime""")
@@ -55,17 +59,18 @@ def prikaz_clanov():
             fun = cur.fetchall()
             c = cur.execute("""SELECT * FROM cin""")
             c = cur.fetchall()
-    return template('prikaz_clanov.html',clani=clani,fun = fun, c=c)
+    return template('prikaz_clanov.html',napaka=napaka,clani=clani,fun = fun, c=c)
 
 @get('/dodaj_clana/')
 def nov_clan():
+     napaka = nastaviSporocilo()
      with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             funkcija= cur.execute("""SELECT * FROM funkcija""")
             funkcija = cur.fetchall()
             cin = cur.execute("""SELECT * FROM cin""")
             cin = cur.fetchall()
-     return template('n_clan',funkcije = funkcija, cin=cin)
+     return template('n_clan',napaka=napaka,funkcije = funkcija, cin=cin)
     
 @post('/dodaj_clana/')
 def nov_clan_post():
@@ -81,18 +86,25 @@ def nov_clan_post():
             funkcija_id = cur.fetchall()
             cin_id = cur.execute(f"""SELECT id_cin FROM cin WHERE cin = %s""",[cin])
             cin_id = cur.fetchall()
-    nov = Clan(int(emso),ime,priimek,funkcija_id[0][0],cin_id[0][0],zdravniski)
-    nov.dodaj_clana()
+    try:
+        nov = Clan(int(emso),ime,priimek,funkcija_id[0][0],cin_id[0][0],zdravniski)
+        nov.dodaj_clana()
+    except:
+         nastaviSporocilo("ta emšo že obstaja")
     redirect('/clani/')
 
 @post('/odstrani_clana/')
 def odstrani_clana():
     emso = request.forms.getunicode('emso')
-    Clan.spremeni_aktivnost(int(emso))
+    try:
+        Clan.spremeni_aktivnost(int(emso))
+    except:
+         nastaviSporocilo("Izbris člana ni uspel")
     redirect('/clani/')
 
 @post('/preusmeritev_popravi_clan/')
 def popravi_clana():
+    napaka = nastaviSporocilo()
     emso = request.forms.getunicode('emso')
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
@@ -102,7 +114,7 @@ def popravi_clana():
         fun = cur.fetchall()
         c = cur.execute("""SELECT * FROM cin""")
         c = cur.fetchall()
-    return template('popravi_clana.html',clan=clan,fun=fun,c=c)
+    return template('popravi_clana.html',napaka=napaka,clan=clan,fun=fun,c=c)
 
 @post('/popravi_clana/')
 def popravi_clana_dokonco():
@@ -118,16 +130,17 @@ def popravi_clana_dokonco():
             funkcija_id = cur.fetchall()
             cin_id = cur.execute(f"""SELECT id_cin FROM cin WHERE cin = %s""",[cin])
             cin_id = cur.fetchall()
-    Clan.popravi_clana(emso,ime,priimek,funkcija_id[0][0],cin_id[0][0],zd)
+    try:
+        Clan.popravi_clana(emso,ime,priimek,funkcija_id[0][0],cin_id[0][0],zd)
+    except:
+         nastaviSporocilo("popravljanje podatkov ni uspelo")
     redirect('/clani/')
-    
-###############################################################################
-# vozila
-###############################################################################   
+###################################################################################################     
 
 
 @get('/vozila/') 
 def prikaz_vozil():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             vozila = cur.execute("""SELECT * FROM vozilo WHERE aktivno = true""")
@@ -136,17 +149,18 @@ def prikaz_vozil():
             tip = cur.fetchall()
             izpit = cur.execute("""SELECT * FROM kategorija_vozniskega_dovoljenja""")
             izpit = cur.fetchall()
-    return template('prikaz_vozil.html',vozila=vozila,tip=tip,izpit=izpit)
+    return template('prikaz_vozil.html',napaka = napaka,vozila=vozila,tip=tip,izpit=izpit)
 
 @get('/dodaj_vozilo/')
 def novo_vozilo():
+    napaka = nastaviSporocilo()
      with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             tip_v = cur.execute("""SELECT * FROM tip_vozila""")
             tip_v = cur.fetchall()
             izpit = cur.execute("""SELECT * FROM kategorija_vozniskega_dovoljenja""")
             izpit = cur.fetchall()
-     return template('novo_vozilo.html',tip_v=tip_v,izpit=izpit)
+     return template('novo_vozilo.html',napaka=napaka,tip_v=tip_v,izpit=izpit)
 
 @post('/dodaj_vozilo/')
 def novo_vozilo_post():
@@ -162,18 +176,25 @@ def novo_vozilo_post():
             izpit_id = cur.fetchall()
             tip_id = cur.execute(f"""SELECT id_vozilo FROM tip_vozila WHERE tip_vozila  = %s""",[tip])
             tip_id = cur.fetchall()
-    nov = Vozilo(reg,tip_id[0][0],izpit_id[0][0],int(potniki),znamka,tehnicni)
-    nov.dodaj_vozilo()
+    try:
+        nov = Vozilo(reg,tip_id[0][0],izpit_id[0][0],int(potniki),znamka,tehnicni)
+        nov.dodaj_vozilo()
+    except:
+         nastaviSporocilo("Ta registerska številka že obstaja")
     redirect('/vozila/')
 
 @post('/odstrani_vozilo/')
 def odstrani_vozilo():
     reg = request.forms.getunicode('reg')
-    Vozilo.spremeni_aktivnost(reg)
+    try:
+        Vozilo.spremeni_aktivnost(reg)
+    except:
+         nastaviSporocilo("izbris vozila ni uspel")
     redirect('/vozila/')
 
 @post('/preusmeritev_popravi_vozilo/')
 def popravi_vozilo():
+    napaka = nastaviSporocilo()
     reg = request.forms.getunicode('reg')
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
@@ -183,7 +204,7 @@ def popravi_vozilo():
         tip_v = cur.fetchall()
         izpit = cur.execute("""SELECT * FROM kategorija_vozniskega_dovoljenja""")
         izpit = cur.fetchall()
-    return template('popravi_vozilo.html',vozilo=vozilo,tip_v=tip_v,izpit=izpit)
+    return template('popravi_vozilo.html',napaka=napaka,vozilo=vozilo,tip_v=tip_v,izpit=izpit)
 
 @post('/popravi_vozilo/')
 def popravi_clana_dokonco():
@@ -199,25 +220,28 @@ def popravi_clana_dokonco():
             izpit_id = cur.fetchall()
             tip_id = cur.execute(f"""SELECT id_vozilo FROM tip_vozila WHERE tip_vozila  = %s""",[tip])
             tip_id = cur.fetchall()
-    Vozilo.popravi_vozilo(reg,tip_id[0][0],izpit_id[0][0],int(potniki),znamka,tehnicni)
+    try:        
+        Vozilo.popravi_vozilo(reg,tip_id[0][0],izpit_id[0][0],int(potniki),znamka,tehnicni)
+    except:
+        nastaviSporocilo("Popravljanje podatkov ni uspelo")
     redirect('/vozila/')
 
-###############################################################################
-# INTERVENCIJE
-###############################################################################
+#######################################################################################
 
 @get("/intervencije/")
 def intervencije():
+      napaka = nastaviSporocilo()
       with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             inte = cur.execute("""SELECT * FROM intervencija""")
             inte = cur.fetchall()
             tip_int = cur.execute("""SELECT * FROM tip_intervencije""")
             tip_int = cur.fetchall()
-      return template("prikaz_int.html",inte=inte, tip_int=tip_int)
+      return template("prikaz_int.html",napaka=napaka,inte=inte,tip_int=tip_int)
 
 @get("/dodaj_int/")
 def dodaj_intervencijo():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
         tip_int = cur.execute("""SELECT * FROM tip_intervencije""")
@@ -233,12 +257,16 @@ def post_dodaj_int():
             cur = baza.cursor()
             tip_id = cur.execute(f"""SELECT id_tipa_intervencije FROM tip_intervencije WHERE tip  = %s""",[tip])
             tip_id = cur.fetchall()
-    nov = Intervencija(opis,datum,tip_id[0][0])
-    nov.dodaj_intervencijo()
-    redirect('/dodaj_clane_na_int/')
+    try:
+        nov = Intervencija(opis,datum,tip_id[0][0])
+        nov.dodaj_intervencijo()
+        redirect('/dodaj_clane_na_int/')
+    except:
+         nastaviSporocilo("Dodajanje intervencije ni uspelo")
 
 @get('/dodaj_clane_na_int/')
 def dodaj_clane_int():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
         clani = cur.execute("""SELECT emso,ime,priimek,funkcija,cin,zdravniski,aktiven FROM clan WHERE aktiven=true ORDER BY priimek,ime """)
@@ -249,7 +277,7 @@ def dodaj_clane_int():
         id_int = cur.fetchall()
         tip_v = cur.execute("""SELECT * FROM tip_vozila""")
         tip_v = cur.fetchall()
-    return template('dodaj_clane_int.html',clani=clani, id_int=id_int,vozila=vozila,tip_v=tip_v)
+    return template('dodaj_clane_int.html',napaka=napaka,clani=clani, id_int=id_int,vozila=vozila,tip_v=tip_v)
 
 @post('/dodaj_clane_na_int/')
 def post():
@@ -272,17 +300,21 @@ def post():
         if r is not None:
             za_dodat_vozila.append(reg[0])    
 
-    for cl_emso in za_dodat_clane:
-        Intervencija.dodaj_clana_intervenciji(int(id_intervencije),cl_emso)
+    try:
+        for cl_emso in za_dodat_clane:
+            Intervencija.dodaj_clana_intervenciji(int(id_intervencije),cl_emso)
 
-    for v_reg in za_dodat_vozila:
-        Intervencija.dodaj_vozilo_intervenciji(int(id_intervencije),v_reg)
+        for v_reg in za_dodat_vozila:
+            Intervencija.dodaj_vozilo_intervenciji(int(id_intervencije),v_reg)
+    except:
+         nastaviSporocilo("dodajane članov in vozil ni dokočno uspelo, izbriši intervencijo in jo ustvari na novo")
 
     redirect("/intervencije/")
 
 
 @route("/prikaz_int/", method='POST')
 def prikaz_intervencije():
+    napaka = nastaviSporocilo()
     id_za_prikaz = request.forms.getunicode('id')
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
@@ -297,12 +329,15 @@ def prikaz_intervencije():
         tip_int = cur.execute("""SELECT * FROM tip_intervencije""")
         tip_int = cur.fetchall()
     
-    return template('podroben_prikaz_int.html',int=int,tip_v=tip_v,tip_int=tip_int,clani_na_int=clani_na_int,vozila_na_int=vozila_na_int)
+    return template('podroben_prikaz_int.html',napaka=napaka,int=int,tip_v=tip_v,tip_int=tip_int,clani_na_int=clani_na_int,vozila_na_int=vozila_na_int)
 
 @route('/odstrani_int/', method='POST')
 def odstrani_intervencijo():
     id = request.forms.getunicode('id')
-    Intervencija.odstrani_intervencijo(int(id))
+    try:
+        Intervencija.odstrani_intervencijo(int(id))
+    except:
+        nastaviSporocilo("Izbris intervencije ni uspel")
     redirect('/intervencije/')
       
 ###############################################################################
@@ -311,21 +346,23 @@ def odstrani_intervencijo():
 
 @get("/tekmovanja/")
 def tekmovanje():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             tek = cur.execute("""SELECT * FROM tekmovanje""")
             tek = cur.fetchall()
             tip_tek = cur.execute("""SELECT * FROM tip_tekmovanja""")
             tip_tek = cur.fetchall()
-    return template("prikaz_tek.html",tek=tek, tip_tek=tip_tek)
+    return template("prikaz_tek.html",napaka=napaka,tek=tek, tip_tek=tip_tek)
 
 @get('/dodaj_tekmovanje/')
 def dodaj_tekmovanje():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
         tip_tekmo = cur.execute("""SELECT * FROM tip_tekmovanja""")
         tip_tekmo = cur.fetchall()
-    return template('novo_tekmovanje.html',tip_tekmo=tip_tekmo)
+    return template('novo_tekmovanje.html',napaka=napaka,tip_tekmo=tip_tekmo)
 
 @route('/dodaj_tekekmovanje/', method='POST')
 def post_dodaj_tekmovanje():
@@ -336,14 +373,20 @@ def post_dodaj_tekmovanje():
             cur = baza.cursor()
             tip_id = cur.execute(f"""SELECT id_tip FROM tip_tekmovanja WHERE tip = %s""",[tip])
             tip_id = cur.fetchall()
-    nov = Tekomvanje(lokacija,tip_id[0][0],datum)
-    nov.dodaj_tekmovanje()
+    try:
+        nov = Tekomvanje(lokacija,tip_id[0][0],datum)
+        nov.dodaj_tekmovanje()
+    except:
+        nastaviSporocilo("Dodajane tekmovanja ni uspelo")
     redirect('/tekmovanja/')
 
 @route('/odstrani_tek/', method='POST')
 def odstrani_tekmovanje():
     id = request.forms.getunicode('id_tek')
-    Tekomvanje.odstrani_tekmovanje(int(id))
+    try:
+        Tekomvanje.odstrani_tekmovanje(int(id))
+    except:
+        nastaviSporocilo("Izbris tekmovanja ni uspel")
     redirect('/tekmovanja/')
 
 ###############################################################################
@@ -352,16 +395,18 @@ def odstrani_tekmovanje():
 
 @get("/vaje/")
 def vaje():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             vaje = cur.execute("""SELECT id,obvezna,tip_vaje,datum,ime,priimek FROM vaja JOIN clan ON vodja=emso""")
             vaje = cur.fetchall()
             tip = cur.execute("""SELECT * FROM tip_intervencije""")
             tip= cur.fetchall()
-    return template("prikaz_vaj.html",vaje=vaje, tip=tip)   
+    return template("prikaz_vaj.html",napaka=napaka,vaje=vaje, tip=tip)   
 
 @get("/dodaj_vajo/")
 def dodaj_vajo():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
         tip_vaje = cur.execute("""SELECT * FROM tip_intervencije""")
@@ -370,7 +415,7 @@ def dodaj_vajo():
         vodaja = cur.fetchall()
         vaje = cur.execute("""SELECT * FROM vaja""")
         vaje = cur.fetchall()
-    return template('nova_vaja.html',tip_vaje=tip_vaje,vodja=vodaja,vaje=vaje) 
+    return template('nova_vaja.html',napaka=napaka,tip_vaje=tip_vaje,vodja=vodaja,vaje=vaje) 
 
 @route('/dodaj_vajo/', method='POST')
 def post_dodaj_vajo():
@@ -386,28 +431,35 @@ def post_dodaj_vajo():
             cur = baza.cursor()
             tip_id = cur.execute(f"""SELECT id_tipa_intervencije FROM tip_intervencije WHERE tip  = %s""",[tip_vaje])
             tip_id = cur.fetchall()
-
-    nov = Vaja(u,tip_id[0][0],int(vodja), datum)
-    nov.dodaj_vajo()
+    try:
+        nov = Vaja(u,tip_id[0][0],int(vodja), datum)
+        nov.dodaj_vajo()
+    except:
+         nastaviSporocilo("Dodajane vaje ni uspelo")
     redirect('/vaje/')
 
 @route('/odstrani_vajo/', method='POST')
 def odstrani_vajo():
-     id = request.forms.getunicode('id_vaje')
-     Vaja.odstrani_vajo(int(id))
-     redirect("/vaje/")
+    id = request.forms.getunicode('id_vaje')
+    try:
+        Vaja.odstrani_vajo(int(id))
+    except:
+        nastaviSporocilo("Izbris vaje ni uspel")
+    redirect("/vaje/")
 
 ###########################################################################
 @get("/oprema/")
 def oprema():
+    napaka = nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
             cur = baza.cursor()
             clani = cur.execute("""SELECT emso,ime,priimek FROM clan WHERE aktiven=true ORDER BY priimek,ime""")
             clani = cur.fetchall()
-    return template("prikaz_clanov_oprema.html",clani=clani)
+    return template("prikaz_clanov_oprema.html",napaka=napaka,clani=clani)
 
 @get("/preusmeritev_pregled_opreme/<emso_za_prikaz>/")
 def prikaz_opreme(emso_za_prikaz):
+    napaka= nastaviSporocilo()
     with psycopg2.connect(conn_string) as baza:
         cur = baza.cursor()
         oprema_clana = cur.execute(f"""SELECT * FROM osebna_oprema WHERE emso_clana = {emso_za_prikaz} """)
@@ -415,15 +467,18 @@ def prikaz_opreme(emso_za_prikaz):
         clan = cur.execute(f"""SELECT emso,ime,priimek FROM clan WHERE emso ={emso_za_prikaz} """)
         clan = cur.fetchall()
     
-    return template('podroben_prikaz_opreme.html',oprema_clana=oprema_clana,clan=clan)
+    return template('podroben_prikaz_opreme.html',napaka=napaka,oprema_clana=oprema_clana,clan=clan)
 
 
 @route("/dodaj_opremo/", method='POST')
 def dodaj_opremo_clanau():
     emso_za_dodajo = request.forms.getunicode('emso')
     oprema = request.forms.getunicode('oprema')
-    nov = Oprema(int(emso_za_dodajo),oprema)
-    nov.dodaj_opremo()
+    try:
+        nov = Oprema(int(emso_za_dodajo),oprema)
+        nov.dodaj_opremo()
+    except:
+         nastaviSporocilo("Dodajanje opreme ni uspelo")
     redirect(f'/preusmeritev_pregled_opreme/{emso_za_dodajo}/')
 
 
@@ -431,7 +486,10 @@ def dodaj_opremo_clanau():
 def prikaz_opreme():
     id_opreme = request.forms.getunicode('id_opreme')
     emso =request.forms.getunicode('emso')
-    Oprema.odstrani_opremo(int(id_opreme))
+    try:
+        Oprema.odstrani_opremo(int(id_opreme))
+    except:
+        nastaviSporocilo("Odstranitev opreme ni uspelo")
     redirect(f"/preusmeritev_pregled_opreme/{emso}/")
 
 
